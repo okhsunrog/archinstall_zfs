314a40e4f63c61f92e6355cdea3dde1f8bbe4325
okhsunrog <me@okhsunrog.dev>
2025-01-02 22:32:28 +0300

    added entry point

--- archinstall_zfs/main.py
+++ archinstall_zfs/main.py
@@ -262,3 +262,7 @@ def main() -> bool:
         perform_installation(disk_manager, zfs_manager)
     except Exception as e:
         error(f"Installation failed: {str(e)}")
+        return False
+
+if __name__ == '__main__':
+    main()
